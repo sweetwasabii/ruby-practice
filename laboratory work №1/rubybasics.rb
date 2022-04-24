3c2cef2ddf0501ed6e5432475eb48ceea51f4126
sweetwasabii <diana122000@gmail.com>
2022-04-24 12:23:03 +0300

    added task 1.2

--- laboratory work №1/rubybasics.rb
+++ laboratory work №1/rubybasics.rb
@@ -3,4 +3,18 @@
 Реализовать и вызвать Hello World c комментарием.
 =end
 
-puts "Hello, World!";
+# puts "Hello, World!";
+
+=begin
+№2 Принять имя пользователя как аргумент программы.
+Поздороваться с пользователем с использованием форматирования строки.
+Рассмотреть тип строка, получить все методы, рассмотреть irb режим.
+=end
+
+=begin
+print "Input username: ";
+username = gets.chomp;
+print "Hello, #{username}!";
+=end
+
+# print "Методы класса String: #{String.instance_methods}";
